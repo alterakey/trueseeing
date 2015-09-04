94c7fc0026c8f3b0dab5399edb8ae395005d924b
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2015-09-05 02:35:41 +0900

    WIP

--- tests/test_db.py
+++ tests/test_db.py
@@ -33,12 +33,24 @@ class P:
   @staticmethod
   def parsed(s):
     q = collections.deque(re.split(r'\n+', s))
+    if q:
+      class_sig = P.parsed_as_op(q.popleft())
+      while q:
+        t = P.parsed_as_op(q[0])
+        if t.v in ('super', 'source'):
+          class_sig.p.append(t)
+          q.popleft()
+        else:
+          break
+      yield class_sig
+      yield from P.parsed_whole(q)
+
+  @staticmethod
+  def parsed_whole(q):
     while q:
       l = q.popleft()
       if l:
-        x, xs = P.head_and_tail([t for t in P.lexed_as_smali(l)])
-        if xs is not None:
-          x.p = [t for t in xs]
+        x = P.parsed_as_op(l)
         if x.t != 'directive':
           yield x
         else:
@@ -47,26 +59,18 @@ class P:
           else:
             if x.p is None:
               x.p = []
-            x.p.append(P.lexed_as_annotation_content(q))
+            x.p.append(P.parsed_as_annotation_content(q))
             yield x
 
   @staticmethod
-  def lexed_as_smali(s):
-    for m in re.finditer(r':(?P<label>[a-z0-9_-]+)|{\s*(?P<multilabel>(?::[a-z0-9_-]+(?: .. )*)+\s*)}|\.(?P<directive>[a-z0-9_-]+)|"(?P<string>.*)"|(?P<reg>[vp][0-9]+)|{(?P<multireg>[vp0-9,. ]+)}|(?P<id>[a-z0-9/-]+)|(?P<reflike>[A-Za-z_0-9/;$()<>-]+)|#(?P<comment>.*)', s):
-      key = m.lastgroup
-      value = m.group(key)
-      yield Token(key, value, [])
+  def parsed_as_op(l):
+    x, xs = P.head_and_tail([t for t in P.lexed_as_smali(l)])
+    if xs is not None:
+      x.p = [t for t in xs]
+    return x
 
   @staticmethod
-  def lexed_as_class(q):
-    pass
-
-  @staticmethod
-  def lexed_as_method(q):
-    pass
-
-  @staticmethod
-  def lexed_as_annotation_content(q):
+  def parsed_as_annotation_content(q):
     contents = []
     try:
       while '.end annotation' not in q[0]:
@@ -75,21 +79,17 @@ class P:
       pass
     return Token('content', ''.join(contents), [])
 
-class SmaliDBTest(unittest.TestCase):
-  def normal(self, c):
-    pass
+  @staticmethod
+  def lexed_as_smali(l):
+    for m in re.finditer(r':(?P<label>[a-z0-9_-]+)|{\s*(?P<multilabel>(?::[a-z0-9_-]+(?: .. )*)+\s*)}|\.(?P<directive>[a-z0-9_-]+)|"(?P<string>.*)"|(?P<reg>[vp][0-9]+)|{(?P<multireg>[vp0-9,. ]+)}|(?P<id>[a-z0-9/-]+)|(?P<reflike>[A-Za-z_0-9/;$()<>-]+)|#(?P<comment>.*)', l):
+      key = m.lastgroup
+      value = m.group(key)
+      yield Token(key, value, [])
 
+
+class SmaliDBTest(unittest.TestCase):
   def test_000(self):
     with open(os.path.join(os.path.dirname(__file__), "fixture_0.smali"), "r") as f:
       for t in P.parsed(f.read()):
         print(t)
-
-
-      for o in (r for r in f if r.strip()):
-        try:
-          print(o)
-          print(smali.Insn(o))
-
-        except IndexError:
-          pass
-      assert False
+    assert True
