719e16b76f872e60416414e2a599033b151ee313
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2015-12-26 16:22:11 +0900

    Detecting path component, ipv4 address, host-like constants

--- trueseeing/fingerprint.py
+++ trueseeing/fingerprint.py
@@ -8,6 +8,8 @@ import re
 from trueseeing.context import warning_on
 from trueseeing.smali import OpMatcher, InvocationPattern, DataFlows
 
+import pkg_resources
+
 def package_name_of(path):
   return os.path.dirname(path).replace('/', '.')
 
@@ -72,26 +74,38 @@ def detect_obfuscator_proguard(context):
       return [warning_on(name=context.apk, row=1, col=0, desc='detected obfuscator: ProGuard', opt='-Wdetect-obfuscator')]
   else:
     return []
-
-def detect_url(context):
+  
+def detect_urllike(context):
   class FakeToken:
     def __init__(self, v, p):
       self.v = v
       self.p = p
+
+  def analyzed(x):
+    if '://' in x:
+      yield dict(type_='URL', value=re.findall(r'\S+://\S+', x))
+    elif re.search(r'^/[{}$%a-zA-Z0-9_-]+(/[{}$%a-zA-Z0-9_-]+)+', x):
+      yield dict(type_='path component', value=re.findall(r'^/[{}$%a-zA-Z0-9_-]+(/[{}$%a-zA-Z0-9_-]+)+', x))
+    elif re.search(r'^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+(:[0-9]+)?$', x):
+      hostlike = re.search(r'^([^:/]+)', x).group(1)
+      components = hostlike.split('.')
+      if len(components) == 4 and all(re.match(r'^\d+$', c) for c in components):
+        yield dict(type_='possible IPv4 address', value=[hostlike])
+      elif re_tlds.search(components[-1]):
+        yield dict(type_='possible FQDN', value=[hostlike])
+
+  with open(pkg_resources.resource_filename(__name__, os.path.join('libs', 'tlds.txt')), 'r') as f:
+    re_tlds = re.compile('^(?:%s)$' % '|'.join(re.escape(l.strip()) for l in f if l and not l.startswith('#')), flags=re.IGNORECASE)
   
   marks = []
   for cl in context.analyzed_classes():
-    # XXX too dumb
-    for k in OpMatcher(cl.ops, InvocationPattern('const-string', '.*https?://.+$')).matching():
-      marks.append(dict(name=context.class_name_of_dalvik_class_type(cl.qualified_name()), method=k.method_, op=k))
+    for k in OpMatcher(cl.ops, InvocationPattern('const-string', '.')).matching():
+      for match in analyzed(k.p[1].v):
+        for v in match['value']:
+          marks.append(dict(name=context.class_name_of_dalvik_class_type(cl.qualified_name()), method=k.method_, op=k, target_val=v, target_type=match['type_']))
   for name, val in context.string_resources():
-    if re.match('https?://.+', val) or re.match('^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+(:[0-9]+)?$', val) or re.match('^/[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)+', val):
-      marks.append(dict(name='resource', method=FakeToken(FakeToken('R.string.%s' % name, []), []), op=FakeToken(None, [None, FakeToken(val, [])])))
+    for match in analyzed(val):
+      for v in match['value']:
+        marks.append(dict(name='resource', method=FakeToken(FakeToken('R.string.%s' % name, []), []), target_val=v, target_type=match['type_']))
       
-  for m in marks:
-    try:
-      m['target_val'] = m['op'].p[1].v
-    except (DataFlows.NoSuchValueError):
-      pass
-
-  return [warning_on(name=m['name'] + '#' + m['method'].v.v, row=0, col=0, desc='detected URL: %s' % m['target_val'], opt='-Wdetect-url') for m in marks]
+  return [warning_on(name=m['name'] + '#' + m['method'].v.v, row=0, col=0, desc='detected %s: %s' % (m['target_type'], m['target_val']), opt='-Wdetect-url') for m in marks]
