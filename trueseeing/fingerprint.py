ce23d1c92d67a57e09f0a7904d963d3f43323fa7
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2016-01-01 20:12:12 +0900

    Refactoring

--- trueseeing/fingerprint.py
+++ trueseeing/fingerprint.py
@@ -10,78 +10,96 @@ from trueseeing.smali import OpMatcher, InvocationPattern, DataFlows
 
 import pkg_resources
 
-def package_name_of(path):
-  return os.path.dirname(path).replace('/', '.')
+class Detector:
+  def __init__(self, context):
+    self.context = context
+  def detect(self):
+    pass
 
-def class_name_of(path):
-  return path.replace('.smali', '').replace('/', '.')
+class LibraryDetector(Detector):
+  def package_name_of(self, path):
+    return os.path.dirname(path).replace('/', '.')
 
-def shared_package_of(c1, c2):
-  o = []
-  try:
-    for a,b in zip(c1.split('.'), c2.split('.')):
-      if a == b:
-        o.append(a)
-      else:
-        break
-  finally:
-    return o
+  def package_family_of(self, p):
+    f = collections.OrderedDict([
+      (r'javax\..*', None),
+      (r'(android\.support\.v[0-9]+)\..*', r'\1'),
+      (r'(com\.google\.android\.gms)\..*', r'\1'),
+      (r'(.*?)\.internal(?:\..*)?$', r'\1'),
+      (r'(.*?)(?:\.[a-z]{,4})+$', r'\1'),
+      (r'([a-z0-9_]{5,}(?:\.[a-z0-9_]{2,})+?)\..*', r'\1'),
+    ])
+    for k, v in f.items():
+      if re.match(k, p):
+        try:
+          return re.sub(k, v, p)
+        except TypeError:
+          return None
+    else:
+      return p
+    
+  def shared_package_of(self, c1, c2):
+    o = []
+    try:
+      for a,b in zip(c1.split('.'), c2.split('.')):
+        if a == b:
+          o.append(a)
+        else:
+          break
+    finally:
+      return o
 
-def is_kind_of(c1, c2):
-  return True if shared_package_of(c1, c2) else False
+  def is_kind_of(self, c1, c2):
+    return True if self.shared_package_of(c1, c2) else False
 
-def package_family_of(p):
-  f = collections.OrderedDict([
-    (r'javax\..*', None),
-    (r'(android\.support\.v[0-9]+)\..*', r'\1'),
-    (r'(com\.google\.android\.gms)\..*', r'\1'),
-    (r'(.*?)\.internal(?:\..*)?$', r'\1'),
-    (r'(.*?)(?:\.[a-z]{,4})+$', r'\1'),
-    (r'([a-z0-9_]{5,}(?:\.[a-z0-9_]{2,})+?)\..*', r'\1'),
-  ])
-  for k, v in f.items():
-    if re.match(k, p):
-      try:
-        return re.sub(k, v, p)
-      except TypeError:
-        return None
-  else:
-    return p
+  def detect(self):
+    package = self.context.parsed_manifest().getroot().xpath('/manifest/@package', namespaces=dict(android='http://schemas.android.com/apk/res/android'))[0]
 
+    packages = dict()
+    for fn in (self.context.source_name_of_disassembled_class(r) for r in self.context.disassembled_classes()):
+      family = self.package_family_of(self.package_name_of(fn))
+      if family is not None:
+        try:
+          packages[family].append(fn)
+        except KeyError:
+          packages[family] = [fn]
+        else:
+          pass
+    packages = {k:v for k,v in packages.items() if not self.is_kind_of(k, package) and re.search(r'\.[a-zA-Z0-9]{4,}(?:\.|$)', k)}
+
+    return [warning_on(name=self.context.apk, row=1, col=0, desc='detected library: %s (score: %d)' % (p, len(packages[p])), opt='-Wdetect-library') for p in sorted(packages.keys())]
+  
 def detect_library(context):
-  package = context.parsed_manifest().getroot().xpath('/manifest/@package', namespaces=dict(android='http://schemas.android.com/apk/res/android'))[0]
+  return LibraryDetector(context).detect()
 
-  packages = dict()
-  for fn in (context.source_name_of_disassembled_class(r) for r in context.disassembled_classes()):
-    family = package_family_of(package_name_of(fn))
-    if family is not None:
-      try:
-        packages[family].append(fn)
-      except KeyError:
-        packages[family] = [fn]
-      else:
-        pass
-  packages = {k:v for k,v in packages.items() if not is_kind_of(k, package) and re.search(r'\.[a-zA-Z0-9]{4,}(?:\.|$)', k)}
+class ProGuardDetector(Detector):
+  def class_name_of(self, path):
+    return path.replace('.smali', '').replace('/', '.')
 
-  return [warning_on(name=context.apk, row=1, col=0, desc='detected library: %s (score: %d)' % (p, len(packages[p])), opt='-Wdetect-library') for p in sorted(packages.keys())]
+  def detect(self):
+    for c in (self.class_name_of(self.context.source_name_of_disassembled_class(r)) for r in self.context.disassembled_classes()):
+      if re.search('(?:^|\.)a$', c):
+        return [warning_on(name=self.context.apk, row=1, col=0, desc='detected obfuscator: ProGuard', opt='-Wdetect-obfuscator')]
+    else:
+      return []
 
 def detect_obfuscators(context):
   return detect_obfuscator_proguard(context)
 
 def detect_obfuscator_proguard(context):
-  for c in (class_name_of(context.source_name_of_disassembled_class(r)) for r in context.disassembled_classes()):
-    if re.search('(?:^|\.)a$', c):
-      return [warning_on(name=context.apk, row=1, col=0, desc='detected obfuscator: ProGuard', opt='-Wdetect-obfuscator')]
-  else:
-    return []
+  return ProGuardDetector(context).detect()
   
-def detect_urllike(context):
-  class FakeToken:
-    def __init__(self, v, p):
-      self.v = v
-      self.p = p
+class FakeToken:
+  def __init__(self, v, p):
+    self.v = v
+    self.p = p
 
-  def analyzed(x):
+class UrlLikeDetector(Detector):
+  def __init__(self, context):
+    super().__init__(context)
+    self.re_tlds = None
+  
+  def analyzed(self, x):
     if '://' in x:
       yield dict(type_='URL', value=re.findall(r'\S+://\S+', x))
     elif re.search(r'^/[{}$%a-zA-Z0-9_-]+(/[{}$%a-zA-Z0-9_-]+)+', x):
@@ -91,21 +109,25 @@ def detect_urllike(context):
       components = hostlike.split('.')
       if len(components) == 4 and all(re.match(r'^\d+$', c) for c in components):
         yield dict(type_='possible IPv4 address', value=[hostlike])
-      elif re_tlds.search(components[-1]):
+      elif self.re_tlds.search(components[-1]):
         yield dict(type_='possible FQDN', value=[hostlike])
+        
+  def detect(self):
+    with open(pkg_resources.resource_filename(__name__, os.path.join('libs', 'tlds.txt')), 'r') as f:
+      self.re_tlds = re.compile('^(?:%s)$' % '|'.join(re.escape(l.strip()) for l in f if l and not l.startswith('#')), flags=re.IGNORECASE)
 
-  with open(pkg_resources.resource_filename(__name__, os.path.join('libs', 'tlds.txt')), 'r') as f:
-    re_tlds = re.compile('^(?:%s)$' % '|'.join(re.escape(l.strip()) for l in f if l and not l.startswith('#')), flags=re.IGNORECASE)
-  
-  marks = []
-  for cl in context.analyzed_classes():
-    for k in OpMatcher(cl.ops, InvocationPattern('const-string', '.')).matching():
-      for match in analyzed(k.p[1].v):
+    marks = []
+    for cl in self.context.analyzed_classes():
+      for k in OpMatcher(cl.ops, InvocationPattern('const-string', '.')).matching():
+        for match in self.analyzed(k.p[1].v):
+          for v in match['value']:
+            marks.append(dict(name=self.context.class_name_of_dalvik_class_type(cl.qualified_name()), method=k.method_, op=k, target_val=v, target_type=match['type_']))
+    for name, val in self.context.string_resources():
+      for match in self.analyzed(val):
         for v in match['value']:
-          marks.append(dict(name=context.class_name_of_dalvik_class_type(cl.qualified_name()), method=k.method_, op=k, target_val=v, target_type=match['type_']))
-  for name, val in context.string_resources():
-    for match in analyzed(val):
-      for v in match['value']:
-        marks.append(dict(name='resource', method=FakeToken(FakeToken('R.string.%s' % name, []), []), target_val=v, target_type=match['type_']))
-      
-  return [warning_on(name=m['name'] + '#' + m['method'].v.v, row=0, col=0, desc='detected %s: %s' % (m['target_type'], m['target_val']), opt='-Wdetect-url') for m in marks]
+          marks.append(dict(name='resource', method=FakeToken(FakeToken('R.string.%s' % name, []), []), target_val=v, target_type=match['type_']))
+
+    return [warning_on(name=m['name'] + '#' + m['method'].v.v, row=0, col=0, desc='detected %s: %s' % (m['target_type'], m['target_val']), opt='-Wdetect-url') for m in marks]
+  
+def detect_urllike(context):
+  return UrlLikeDetector(context).detect()
