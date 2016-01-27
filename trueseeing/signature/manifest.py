101b01c17c3994333a9145cdca8f99c32a3f2381
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2016-01-27 12:34:55 +0900

    Enclosing detection logic in detector classes

--- trueseeing/signature/manifest.py
+++ trueseeing/signature/manifest.py
@@ -11,25 +11,43 @@ import itertools
 import logging
 
 from trueseeing.context import warning_on
+from trueseeing.signature.base import Detector
 
 log = logging.getLogger(__name__)
 
+class ManifestOpenPermissionDetector(Detector):
+  def detect(self):
+    # TBD: compare with actual permission needs
+    return [warning_on(name='AndroidManifest.xml', row=1, col=0, desc='open permissions: %s' % p, opt='-Wmanifest-open-permission') for p in self.context.permissions_declared()]
+
+class ManifestMissingPermissionDetector(Detector):
+  def detect(self):
+    # TBD: compare with actual permission needs
+    return []
+
+class ManifestManipActivity(Detector):
+  def detect(self):
+    return [warning_on(name='AndroidManifest.xml', row=1, col=0, desc='manipulatable Activity: %s' % name, opt='-Wmanifest-manip-activity') for name in set(itertools.chain(
+      self.context.parsed_manifest().getroot().xpath('//activity[not(@android:permission)]/intent-filter/../@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
+      self.context.parsed_manifest().getroot().xpath('//activity[not(@android:permission) and (@android:exported="true")]/@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
+    ))]
+
+class ManifestManipBroadcastReceiver(Detector):
+  def detect(self):
+    return [warning_on(name='AndroidManifest.xml', row=1, col=0, desc='manipulatable BroadcastReceiver: %s' % name, opt='-Wmanifest-manip-broadcastreceiver') for name in set(itertools.chain(
+      self.context.parsed_manifest().getroot().xpath('//receiver[not(@android:permission)]/intent-filter/../@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
+      self.context.parsed_manifest().getroot().xpath('//receiver[not(@android:permission) and (@android:exported="true")]/@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
+    ))]
+  
+  
 def check_manifest_open_permission(context):
-  # TBD: compare with actual permission needs
-  return [warning_on(name='AndroidManifest.xml', row=1, col=0, desc='open permissions: %s' % p, opt='-Wmanifest-open-permission') for p in context.permissions_declared()]
+  return ManifestOpenPermissionDetector(context).detect()
 
 def check_manifest_missing_permission(context):
-  # TBD: compare with actual permission needs
-  return []
+  return ManifestMissingPermissionDetector(context).detect()
 
 def check_manifest_manip_activity(context):
-  return [warning_on(name='AndroidManifest.xml', row=1, col=0, desc='manipulatable Activity: %s' % name, opt='-Wmanifest-manip-activity') for name in set(itertools.chain(
-    context.parsed_manifest().getroot().xpath('//activity[not(@android:permission)]/intent-filter/../@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
-    context.parsed_manifest().getroot().xpath('//activity[not(@android:permission) and (@android:exported="true")]/@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
-  ))]
+  return ManifestManipActivity(context).detect()
 
 def check_manifest_manip_broadcastreceiver(context):
-  return [warning_on(name='AndroidManifest.xml', row=1, col=0, desc='manipulatable BroadcastReceiver: %s' % name, opt='-Wmanifest-manip-broadcastreceiver') for name in set(itertools.chain(
-    context.parsed_manifest().getroot().xpath('//receiver[not(@android:permission)]/intent-filter/../@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
-    context.parsed_manifest().getroot().xpath('//receiver[not(@android:permission) and (@android:exported="true")]/@android:name', namespaces=dict(android='http://schemas.android.com/apk/res/android')),
-  ))]
+  return ManifestManipBroadcastReceiver(context).detect()
