c504d8866c5656dec84d071f4f9937499c62729b
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2016-02-09 16:40:54 +0900

    Classifing X509 artifacts with crude logic

--- trueseeing/signature/crypto.py
+++ trueseeing/signature/crypto.py
@@ -71,11 +71,15 @@ class CryptoStaticKeyDetector(Detector):
           pass
 
   def do_detect_case2(self):
+    # XXX: Crude detection
+    def should_be_secret(k, val):
+      return any(x in k.method_.qualified_name().lower() for x in ['inapp','billing','iab','sku','store'])
+
     pat = '^MI[IG][0-9A-Za-z+/=-]{32,}AQAB'
     for cl in self.context.analyzed_classes():
       for k in OpMatcher(cl.ops, InvocationPattern('const-string', pat)).matching():
         val = k.p[1].v
-        yield self.issue(IssueSeverity.SEVERE, IssueConfidence.TENTATIVE, '%(name)s#%(method)s' % dict(name=self.context.class_name_of_dalvik_class_type(cl.qualified_name()), method=k.method_.v.v), 'insecure cryptography: static keys: "%(target_val)s" [%(target_val_len)d] (X.509)' % dict(target_val=val, target_val_len=len(val)))
+        yield self.issue(IssueSeverity.SEVERE, {True:IssueConfidence.FIRM, False:IssueConfidence.TENTATIVE}[should_be_secret(k, val)], '%(name)s#%(method)s' % dict(name=self.context.class_name_of_dalvik_class_type(cl.qualified_name()), method=k.method_.v.v), 'insecure cryptography: static keys: "%(target_val)s" [%(target_val_len)d] (X.509)' % dict(target_val=val, target_val_len=len(val)))
     for name, val in self.context.string_resources():
       if re.match(pat, val):
         yield self.issue(IssueSeverity.SEVERE, IssueConfidence.TENTATIVE, 'R.string.%s' % name, 'insecure cryptography: static keys: "%(target_val)s" [%(target_val_len)d] (X.509)' % dict(target_val=val, target_val_len=len(val)))
