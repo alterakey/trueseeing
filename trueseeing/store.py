7563c1668b988c5fd5f0a46068364696329cf88e
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2016-07-22 02:39:16 +0900

    Dropping LevelDB

--- trueseeing/store.py
+++ trueseeing/store.py
@@ -1,54 +1,6 @@
 import random
 import hashlib
 import os.path
-
-import plyvel
-
-class LevelDBStore:
-  def __init__(self, path):
-    self.path = os.path.join(path, 'store')
-    self.db = plyvel.DB(self.path, create_if_missing=True)
-
-  def token_bucket(self, name):
-    return LevelDBTokenBucket(self.db, name)
-
-class LevelDBTokenBucket:
-  def __init__(self, db, prefix):
-    self.db = db
-    self.prefix = prefix + b'.'
-    self.prefix_types = self.prefix + b'types.'
-    self.prefix_values = self.prefix + b'values.'
-    self.prefix_refs = self.prefix + b'refs.'
-
-  def get(self, k):
-    return Token(self.db.get(self.prefix_types + k), self.db.get(self.prefix_values + k))
-
-  def put(self, k, token):
-    k = int(k)
-    key = ('%d' % k).encode('ascii')
-    with self.db.write_batch() as b:
-      b.put(self.prefix_types + key, token.t.encode('ascii'))
-      b.put(self.prefix_values + key, token.v.encode('ascii'))
-
-  def append(self, token):
-    k = int(self.db.get(self.prefix + b'KEY', b'0'))
-    key = ('%d' % k).encode('ascii')
-    new_key = ('%d' % (k + 1)).encode('ascii')
-    with self.db.write_batch() as b:
-      b.put(self.prefix + b'KEY', new_key)
-      b.put(self.prefix_types + key, token.t.encode('ascii'))
-      b.put(self.prefix_values + key, token.v.encode('ascii'))
-
-  def delete(self, k):
-    self.types.delete(k)
-    self.values.delete(k)
-
-  def __enter__(self):
-    return self
-
-  def __exit__(self, exc_type, exc_value, traceback):
-    pass
-
 import sqlite3
 
 class SQLite3Store:
