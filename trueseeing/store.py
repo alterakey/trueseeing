be536305ebf1de689f378a8581c10b877d046e28
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2016-07-22 13:14:24 +0900

    Writing op graph

--- trueseeing/store.py
+++ trueseeing/store.py
@@ -2,6 +2,7 @@ import random
 import hashlib
 import os.path
 import sqlite3
+import itertools
 
 class SQLite3Store:
   def __init__(self, path):
@@ -25,7 +26,10 @@ class SQLite3TokenBucket(SQLite3Attachable):
   def __init__(self, db, prefix):
     super().__init__(db)
     self.prefix = prefix.decode('ascii')
-    for s in ['create table if not exists %(prefix)s (id integer primary key, t string, v string)']:
+    for s in [
+        'create table if not exists %(prefix)s (id integer primary key, t string not null, v string not null)',
+        'create table if not exists %(prefix)s_p (id integer not null, idx integer not null, p integer not null)'
+    ]:
       self.db.execute(s % dict(prefix=self.prefix))
 
   def get(self, k):
@@ -34,12 +38,20 @@ class SQLite3TokenBucket(SQLite3Attachable):
 
   def put(self, k, token):
     self.db.execute('replace into %(prefix)s(id,t,v) values (?,?,?)' % dict(prefix=self.prefix), (k, token.t.encode('ascii'), token.v.encode('ascii')))
+    self.db.execute('delete from %(prefix)s_p where id=?' % dict(prefix=self.prefix), (k))
+    self.db.executemany('insert into %(prefix)s_p(id, idx, p) values (?,?,?)' % dict(prefix=self.prefix), ((k,i,token.p[i]) for i in range(len(token.p))))
 
   def append(self, token):
-    self.db.execute('insert into %(prefix)s(t,v) values (?,?)' % dict(prefix=self.prefix), (token.t.encode('ascii'), token.v.encode('ascii')))
+    ids = []
+    for t in itertools.chain([token], token.p):
+      self.db.execute('insert into %(prefix)s(t,v) values (?,?)' % dict(prefix=self.prefix), (t.t.encode('ascii'), t.v.encode('ascii')))
+      for r in self.db.execute('select id from %(prefix)s where rowid=last_insert_rowid() limit 1' % dict(prefix=self.prefix)):
+        ids.append(r[0])
+    self.db.executemany('insert into %(prefix)s_p(id, idx, p) values (?,?,?)' % dict(prefix=self.prefix), ((ids[0], i, ids[i + 1]) for i in range(len(token.p))))
 
   def delete(self, k):
     self.db.execute('delete from %(prefix)s where id=?' % dict(prefix=self.prefix), (k))
+    self.db.execute('delete from %(prefix)s_p where id=? or p=?' % dict(prefix=self.prefix), (k, k))
 
 Store = SQLite3Store
 TokenBucket = SQLite3TokenBucket
