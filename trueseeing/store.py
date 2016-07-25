6b99b517c606f75df17d404f2d7ccd9bbc87e166
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2016-07-25 18:51:50 +0900

    Manually allocating PK in ops

--- trueseeing/store.py
+++ trueseeing/store.py
@@ -10,7 +10,7 @@ class Store:
     for s in [
         'create table if not exists ops (op integer primary key, t varchar not null, v varchar not null)',
         'create table if not exists ops_p (op integer not null, idx integer not null, p integer not null)',
-        'create table if not exists ops_method (op integer not null, method integer not null)',
+        'create table if not exists ops_method (op integer primary key, method integer not null)',
         'create table if not exists ops_class (op integer not null, class integer not null)',
         'create index ops_method_method on ops_method (method)',
         'create index ops_class_class on ops_class (class)',
@@ -30,14 +30,18 @@ class Store:
       return Token(t, v)
 
   def op_append(self, op):
-    ids = []
-    for t in itertools.chain([op], op.p):
-      self.db.execute('insert into ops(t,v) values (?,?)', (t.t, t.v))
-      for r in self.db.execute('select op from ops where rowid=last_insert_rowid() limit 1'):
-        ids.append(r[0])
-    self.db.executemany('insert into ops_p(op, idx, p) values (?,?,?)', ((ids[0], i, ids[i + 1]) for i in range(len(op.p))))
-    for t, id_ in zip(itertools.chain([op], op.p), ids):
-      t._id = id_
+    unused_id = None
+    for r in self.db.execute('select max(op) from ops'):
+      if r[0] is not None:
+        unused_id = r[0] + 1
+      else:
+        unused_id = 1
+    vec = tuple([op] + op.p)
+    for t, idx in zip(vec, range(len(vec))):
+      t._idx = idx
+      t._id = unused_id + idx
+    self.db.executemany('insert into ops(op,t,v) values (?,?,?)', ((t._id, t.t, t.v) for t in vec))
+    self.db.executemany('insert into ops_p(op, idx, p) values (?,?,?)', ((op._id, t._idx, t._id) for t in vec[1:]))
 
   def op_mark_method(self, ops, method):
     self.db.executemany('insert into ops_method(op,method) values (?,?)', ((str(o._id), str(method._id)) for o in ops))
