1ada3f205de25a2af74ca89be40a50c2c524fd2c
Takahiro Yoshimura <altakey@gmail.com>
2016-11-01 02:23:29 +0900

    Dropping rowid

--- trueseeing/libs/store.0.sql
+++ trueseeing/libs/store.0.sql
@@ -1,8 +1,8 @@
 begin exclusive;
 
-create table ops (op integer primary key, t varchar not null, v varchar not null);
-create table ops_p (op integer not null, idx integer not null, p integer primary key);
-create table ops_method (op integer primary key, method integer not null);
-create table ops_class (op integer primary key, class integer not null);
+create table ops (op integer primary key, t varchar not null, v varchar not null) without rowid;
+create table ops_p (op integer not null, idx integer not null, p integer primary key) without rowid;
+create table ops_method (op integer primary key, method integer not null) without rowid;
+create table ops_class (op integer primary key, class integer not null) without rowid;
 
 commit;
