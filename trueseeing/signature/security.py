55a131a75e5d28e91eae771d94cda17d2749a937
Takahiro Yoshimura <altakey@gmail.com>
2016-11-02 13:14:57 +0900

    Restored insecure webview javascript interface detector

--- trueseeing/signature/security.py
+++ trueseeing/signature/security.py
@@ -142,7 +142,7 @@ class SecurityArbitraryWebViewOverwriteDetector(Detector):
               yield self.issue(IssueSeverity.MEDIUM, IssueConfidence.TENTATIVE, self.context.source_name_of_disassembled_resource(fn), 'arbitrary WebView content overwrite: {0} (score: {1:.02f})'.format(t.attrib['{0}id'.format(self.xmlns_android)], size))
 
 class SecurityInsecureWebViewDetector(Detector):
-  #option = 'security-insecure-webview'
+  option = 'security-insecure-webview'
 
   xmlns_android = '{http://schemas.android.com/apk/res/android}'
 
@@ -154,33 +154,31 @@ class SecurityInsecureWebViewDetector(Detector):
       return default
 
   def do_detect(self):
-    targets = {'WebView','XWalkView','GeckoView'}
-    seed = '|'.join(targets)
+    with self.context.store() as store:
+      targets = {'WebView','XWalkView','GeckoView'}
 
-    more = True
-    while more:
-      more = False
-      for cl in (c for c in self.context.analyzed_classes() if (c.super_.v in targets) or (re.search(seed, c.super_.v))):
-        name = self.context.class_name_of_dalvik_class_type(cl.qualified_name())
-        if name not in targets:
-          targets.add(self.context.class_name_of_dalvik_class_type(cl.qualified_name()))
-          more = True
+      more = True
+      while more:
+        more = False
+        for cl in store.query().related_classes('|'.join(targets)):
+          name = store.query().class_name_of(cl)
+          if name not in targets:
+            targets.add(name)
+            more = True
 
-    # XXX: Crude detection
-    for cl in self.context.analyzed_classes():
-      p = self.first(OpMatcher(cl.ops, InvocationPattern('invoke-virtual', 'Landroid/webkit/WebSettings;->setJavaScriptEnabled')).matching())
-      try:
-        if p and DataFlows.solved_constant_data_in_invocation(p, 0):
-          for target in targets:
-            q = self.first(OpMatcher(cl.ops, InvocationPattern('invoke-virtual', 'L(%s);->addJavascriptInterface' % target)).matching())
-            if q:
-              try:
-                if DataFlows.solved_constant_data_in_invocation(q, 0):
-                  yield self.issue(IssueSeverity.MAJOR, IssueConfidence.FIRM, '%s#%s' % (self.context.class_name_of_dalvik_class_type(cl.qualified_name()), q.method_.v.v), 'insecure Javascript interface')
-              except (DataFlows.NoSuchValueError):
-                yield self.issue(IssueSeverity.MAJOR, IssueConfidence.TENTATIVE, '%s#%s' % (self.context.class_name_of_dalvik_class_type(cl.qualified_name()), q.method_.v.v), 'insecure Javascript interface')
-      except (DataFlows.NoSuchValueError):
-        pass
+      # XXX: Crude detection
+      for p in store.query().invocations(InvocationPattern('invoke-virtual', 'Landroid/webkit/WebSettings;->setJavaScriptEnabled')):
+        try:
+          if DataFlows.solved_constant_data_in_invocation(store, p, 0):
+            for target in targets:
+              for q in store.query().invocations_in_class(p, InvocationPattern('invoke-virtual', '%s->addJavascriptInterface' % target)):
+                try:
+                  if DataFlows.solved_constant_data_in_invocation(store, q, 0):
+                    yield self.issue(IssueSeverity.MAJOR, IssueConfidence.FIRM, store.query().qualname_of(q), 'insecure Javascript interface')
+                except (DataFlows.NoSuchValueError):
+                  yield self.issue(IssueSeverity.MAJOR, IssueConfidence.TENTATIVE, store.query().qualname_of(q), 'insecure Javascript interface')
+        except (DataFlows.NoSuchValueError):
+          pass
 
 class FormatStringDetector(Detector):
   option = 'security-format-string'
