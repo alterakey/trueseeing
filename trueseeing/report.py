ae7a3a5b3ea05b3db983b09b6676e6c55d345c0d
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2017-01-10 15:01:27 +0900

    Applying Bootstrap

--- trueseeing/report.py
+++ trueseeing/report.py
@@ -80,7 +80,7 @@ class HTMLReportGenerator(ReportGenerator):
       issues = []
       for row, no in zip(db.execute('select distinct detector, summary, synopsis, description, seealso, solution, cvss3_score, cvss3_vector from analysis_issues order by cvss3_score desc'), range(1, 2**32)):
         instances = []
-        issues.append(dict(no=no, detector=row[0], summary=row[1].title(), synopsis=row[2], description=row[3], seealso=row[4], solution=row[5], cvss3_score=row[6], cvss3_vector=row[7], severity=CVSS3Scoring.severity_of(row[6]).title(), instances=instances))
+        issues.append(dict(no=no, detector=row[0], summary=row[1].title(), synopsis=row[2], description=row[3], seealso=row[4], solution=row[5], cvss3_score=row[6], cvss3_vector=row[7], severity=CVSS3Scoring.severity_of(row[6]).title(), instances=instances, severity_panel_style={'critical':'panel-danger', 'high':'panel-warning', 'medium':'panel-warning', 'low':'panel-success', 'info':'panel-info'}[CVSS3Scoring.severity_of(row[6])]))
         for m in db.execute('select * from analysis_issues where detector=:detector and summary=:summary and cvss3_score=:cvss3_score', {v:row[k] for k,v in {0:'detector', 1:'summary', 6:'cvss3_score'}.items()}):
           issue = Issue.from_analysis_issues_row(m)
           instances.append(dict(info=issue.brief_info(), source=issue.source, row=issue.row, col=issue.col))
