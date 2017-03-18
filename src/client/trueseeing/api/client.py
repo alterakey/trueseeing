d18147b980bd875ab5649bb45631141279f973ac
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2017-03-19 01:37:29 +0900

    Accepting apk file in argument

--- src/client/trueseeing/api/client.py
+++ src/client/trueseeing/api/client.py
@@ -20,10 +20,10 @@ async def msg(websocket):
             else:
                 raise
 
-async def send(websocket):
+async def send(websocket, f):
     while True:
         try:
-            block = sys.stdin.buffer.read(1024 * 1024)
+            block = f.read(1024 * 1024)
             if block:
                 await websocket.send(block)
                 await asyncio.sleep(_YIELD)
@@ -47,17 +47,18 @@ async def complete_either(a, b):
         if exc is not None:
             raise exc
 
-async def hello(host, port):
+async def hello(host, port, target):
     if KEY is not None:
         key = {'X-Trueseeing2-Key':KEY}
     else:
         key = None
-    context = ssl.create_default_context()
-    context.load_verify_locations(cafile=certifi.where())
-    async with websockets.connect('wss://%s:%d/analyze' % (host, port), extra_headers=key, ssl=context) as websocket:
-        sys.stderr.write(await websocket.recv())
-        await complete_either(msg(websocket), send(websocket))
-        await msg(websocket)
+    with open(target, 'rb') as f:
+        context = ssl.create_default_context()
+        context.load_verify_locations(cafile=certifi.where())
+        async with websockets.connect('wss://%s:%d/analyze' % (host, port), extra_headers=key, ssl=context) as websocket:
+            sys.stderr.write(await websocket.recv())
+            await complete_either(msg(websocket), send(websocket, f))
+            await msg(websocket)
 
 def shell():
     import sys
@@ -71,7 +72,7 @@ def shell():
     configfile = os.path.join(os.environ['HOME'], '.trueseeing2', 'config')
     connect_to = dict(host='trueseeing.io', port=443)
 
-    opts, _ = getopt.getopt(sys.argv[1:], 'dc:p:', ['debug', 'config=', 'port='])
+    opts, targets = getopt.getopt(sys.argv[1:], 'dc:p:', ['debug', 'config=', 'port='])
     for o, a in opts:
         if o in ['-d', '--debug']:
             debug = logging.DEBUG
@@ -86,6 +87,10 @@ def shell():
             else:
                 connect_to['port'] = int(a)
 
+    if len(targets) != 1:
+        sys.stderr.write("%(me)s: usage: %(me)s <apk>\n" % dict(me=sys.argv[0]))
+        sys.exit(2)
+
     if configfile_required and not os.path.exists(configfile):
         sys.stderr.write('%s: config file is not found\n', sys.argv[0])
         sys.exit(1)
@@ -98,7 +103,7 @@ def shell():
     except KeyError:
         pass
 
-    asyncio.get_event_loop().run_until_complete(hello(connect_to['host'], connect_to['port']))
+    asyncio.get_event_loop().run_until_complete(hello(connect_to['host'], connect_to['port'], targets[0]))
 
 if __name__ == '__main__':
     shell()
