24c440275cf22d74e4a3c72bb90350655ef32833
Junki Kizuka <junki_k@monolithworks.co.jp>
2019-02-16 21:53:40 +0900

    check interpreter version.

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-import os
-import sys
-import shutil
-
 from setuptools import setup, find_packages
 
 metadata = dict(
@@ -25,7 +21,7 @@ README = open('README.rst').read()
 setup(
   long_description=README,
   packages=find_packages(),
-  package_data={'trueseeing':['libs/*.jar', 'libs/*.txt', 'libs/*.sql', 'template/*']},
+  package_data={'trueseeing': ['libs/*.jar', 'libs/*.txt', 'libs/*.sql', 'template/*']},
   include_package_data=True,
   zip_safe=False,
   install_requires=[
@@ -36,7 +32,7 @@ setup(
   setup_requires=[
     "wheel",
   ],
-  entry_points={'console_scripts':['trueseeing = trueseeing.shell:shell']},
-  **metadata,
-  python_requires='>=3'
+  entry_points={'console_scripts': ['trueseeing = trueseeing._dummy:shell']},
+  python_requires='>=3',
+  **metadata
 )
