146f0bad9bb7af3c1af0a8b8f8fdbbdcb88c4726
Junki Kizuka <k.falconws@gmail.com>
2019-06-20 15:36:34 +0900

    add few test

---
+++ trueseeing/tests/signature/test_security.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestSecurityTamperableWebViewDetector(unittest.TestCase):
+  def test_do_detect(self):
+    # TODO
+    self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ trueseeing/tests/test_trueseeing.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import unittest
+
+import trueseeing.shell
+
+
+class TestTrueseeing(unittest.TestCase):
+  def test_trueseeing(self):
+    os.chdir(os.path.dirname(__file__))
+    sys.argv.append('libs/Android-InsecureBankv2/InsecureBankv2.apk')
+    trueseeing.shell.shell()
+
+    # TODO assert count of severity
+
+
+if __name__ == '__main__':
+    unittest.main()
