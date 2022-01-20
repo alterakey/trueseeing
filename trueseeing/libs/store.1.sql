c1d279a0fe7af9505e929eeb2fc6e385d704cda0
Takahiro Yoshimura <takahiro_y@monolithworks.co.jp>
2022-01-20 15:16:38 +0900

    Experimentally laxing reflike

--- trueseeing/libs/store.1.sql
+++ trueseeing/libs/store.1.sql
@@ -43,7 +43,8 @@ create table class_class_name(class integer primary key, class_name varchar not 
 insert into class_class_name select op as class, coalesce(v9,v8,v7,v6,v5,v4,v3,v2,v1,v) as class_name from op_vecs where t='directive' and v='class';
 
 create table method_method_name (method integer primary key, method_name varchar not null);
-insert into method_method_name select method, (case when name='constructor' then '' else name end||sig) as method_name from (select method, (select v from ops where op=sigop) as sig, (select v from ops where op=sigop-1) as name from (select op as method, coalesce(op9,op8,op7,op6,op5,op4,op3,op2,op1,op) as sigop from op_vecs where t='directive' and v='method') as A) as AA;
+-- XXX: not only non-exhaustive but also can hinder analysis, need a better way
+insert into method_method_name select method, (case when name in ('constructor','public','private','protected','static','final','abstract','synthetic','const','mutable') then '' else name end||sig) as method_name from (select method, (select v from ops where op=sigop) as sig, (select v from ops where op=sigop-1) as name from (select op as method, coalesce(op9,op8,op7,op6,op5,op4,op3,op2,op1,op) as sigop from op_vecs where t='directive' and v='method') as A) as AA;
 
 create table method_qualname (method integer primary key, qualname varchar not null);
 insert into method_qualname select method, (class_name||'->'||method_name) as qualname from methods_class join class_class_name using (class) join method_method_name using (method);
