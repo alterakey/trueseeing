45bc5b9851a931663a9d404751e8d9b9ede752e6
Takahiro Yoshimura <takahiro_y@monolithworks.co.jp>
2022-11-28 11:20:27 +0900

    Refactoring around flagging issues

--- trueseeing/signature/base.py
+++ trueseeing/signature/base.py
@@ -20,9 +20,12 @@ from typing import TYPE_CHECKING
 
 from abc import ABC, abstractmethod
 
+from pubsub import pub
+
 if TYPE_CHECKING:
   from typing import ClassVar
   from trueseeing.core.context import Context
+  from trueseeing.core.issue import Issue
 
 class Detector(ABC):
   option: ClassVar[str]
@@ -32,6 +35,9 @@ class Detector(ABC):
 
   def __init__(self, context: Context) -> None:
     self._context = context
+
+  def _raise_issue(self, issue: Issue) -> None:
+    pub.sendMessage('issue', issue=issue)
 
   @abstractmethod
   async def detect(self) -> None: ...
